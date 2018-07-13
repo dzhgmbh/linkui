bb86fd99872379fe84302380d2b793f2f4e6dc7d
Norman Neurath <n.neurath@optadata-gruppe.de>
2018-07-13 11:16:23 +0200

    Update Bootstrap V4.1.1 and Bugfixes

--- gulpfile.js
+++ gulpfile.js
@@ -19,11 +19,50 @@ gulp.task('js', function() {
         .pipe(gulp.dest('dist'));
 });
 
+//jquery
+gulp.task('jquery', function() {
+    return gulp.src([
+            'node_modules/jquery/dist/jquery.min.js'
+        ])
+        .pipe(gulp.dest('dist/plugins/jquery'));
+});
+
+//Plugin flatpickr
+gulp.task('popper', function() {
+    return gulp.src([
+            'node_modules/popper.js/dist/umd/popper.min.js',
+            'node_modules/popper.js/dist/umd/popper.min.js.map'
+        ])
+        .pipe(gulp.dest('dist/plugins/popper'));
+});
+
+//Plugin flatpickr
+gulp.task('flatpickr', function() {
+    return gulp.src([
+            'node_modules/flatpickr/dist/flatpickr.min.js',
+            'node_modules/flatpickr/dist/l10n/de.js',
+            'node_modules/flatpickr/dist/flatpickr.min.css'
+        ])
+        .pipe(gulp.dest('dist/plugins/flatpickr'));
+});
+
+//Plugin toastr
+gulp.task('toastr', function() {
+    return gulp.src([
+            'node_modules/toastr/build/toastr.min.js',
+            'node_modules/toastr/build/toastr.min.css',
+            'node_modules/toastr/build/toastr.js.map'
+        ])
+        .pipe(gulp.dest('dist/plugins/toastr'));
+});
+
+//icon export
 gulp.task('icons', function() {
     return gulp.src('src/icons/*')
         .pipe(gulp.dest('dist/icons'));
 });
 
+//css
 gulp.task('css', function() {
     return gulp.src('src/scss/linkui.scss')
         .pipe(concat('linkui.min.css'))
@@ -35,6 +74,5 @@ gulp.task('css', function() {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', ['css', 'js', 'icons']);
-
+gulp.task('build', ['css', 'js', 'icons', 'flatpickr', 'toastr', 'jquery', 'popper']);
 gulp.task('default', ['build', 'js', 'watch']);
